be2f50aaa8ba090b2c116d3df12cb682d6956087
Aulia Widyaputra <aulia.widyaputra@gmail.com>
2019-10-26 14:04:09 +0700

    Updates

---
+++ precache-manifest.101557bf383b248a935dfc5127d84198.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e59433243b23e90c958a0e6b334f19c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5f584876cc9baf9e4afb",
+    "url": "/monsters-rolodex/static/css/main.02ed47bc.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "5f584876cc9baf9e4afb",
+    "url": "/monsters-rolodex/static/js/main.2dafd18d.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
